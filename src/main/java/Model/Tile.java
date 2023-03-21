f2afa5f1c814198d71359d70c7f597455c944ebd
matteobettiati <matteo.bet.38@gmail.com>
2023-03-21 15:29:44 +0100

    implemented board,bag,tile,cell,color classes

--- src/main/java/Model/Tile.java
+++ src/main/java/Model/Tile.java
@@ -2,5 +2,14 @@ package Model;
 
 public class Tile {
 
+    private Color color;
+
+    public Tile(Color color) {
+        this.color = color;
+    }
+
+    public Color getTileColor() {
+        return color;
+    }
 
 }
