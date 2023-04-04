8ad1c1a005f6091918e39174d4a241d43eb375fc
matteobettiati <matteo.bet.38@gmail.com>
2023-04-04 17:13:06 +0200

    fixed board tests and methods

--- src/main/java/Server/Model/Board.java
+++ src/main/java/Server/Model/Board.java
@@ -3,6 +3,7 @@ package Server.Model;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import Server.Exception.Board.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,34 +20,29 @@ public class Board {
     public Board(JsonObject board_json, Bag bag) {
         this.matrix_size = board_json.get("matrix.size").getAsInt();
         this.board = new Cell[matrix_size][matrix_size];
+        for(int i=0;i<matrix_size;i++){
+            for(int j=0;j<matrix_size;j++){
+                this.board[i][j] = new Cell();
+            }
+        }
         int board_size = board_json.get("board.size").getAsInt();
         List<Integer> cell_value = getValueList(board_json.getAsJsonArray("cell.value"));
         List<Tile> toDeploy = bag.draw(board_size);
-        int rowIndex=0;
-        int columnIndex=0;
-        for (Integer integer : cell_value) {
-            if(cell_value.get(integer) == 1) {
-                this.board[rowIndex][columnIndex].setStatus(true);
-                this.board[rowIndex][columnIndex].setTile(toDeploy.remove(0));
-            }else{
-                this.board[rowIndex][columnIndex].setStatus(false);
-                this.board[rowIndex][columnIndex].setTile(null);
-            }
-            columnIndex++;
-            if (columnIndex == matrix_size-1) {
-                columnIndex = 0;
-                rowIndex++;
+        int k=0;
+        for (int i=0;i<matrix_size;i++) {
+            for(int j=0;j<matrix_size;j++){
+                if(cell_value.get(k) == 1) {
+                    this.board[i][j].setStatus(true);
+                    this.board[i][j].setTile(toDeploy.remove(0));
+                }else{
+                    this.board[i][j].setStatus(false);
+                    this.board[i][j].setTile(null);
+                }
+                k++;
             }
         }
     }
-    private static  List<Integer> getValueList (JsonArray json){
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i = 1; i <=json.size(); i++)
-            list.add(json.get(json.size()-i).getAsInt());
-        return list;
-    }
-
-    public void convalidateMove(List<Coordinates> coordinates) throws NoValidMoveException {
+    public void convalidateMove(@NotNull List<Coordinates> coordinates) throws NoValidMoveException {
         boolean validMove = false;
         if(coordinates.size() == 1){
             validMove = checkSpaceTile(coordinates.get(0));
@@ -71,7 +67,6 @@ public class Board {
             }else{
                 throw new NullTileException(coordinate);
             }
-
         }
         return result;
     }
@@ -108,8 +103,16 @@ public class Board {
         if(lonelyTile){
             if(tilesTaken > bag.getLastTiles()){
                 throw new CantRefillBoardException ();
-            }
+            }else setTiles(bag,tilesTaken);
         }
+    }
+
+
+    private static  List<Integer> getValueList (JsonArray json){
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 1; i <=json.size(); i++)
+            list.add(json.get(json.size()-i).getAsInt());
+        return list;
     }
 
     private Cell getCell(Coordinates coordinates) {
@@ -140,20 +143,22 @@ public class Board {
     private boolean checkSpaceTile(Coordinates c1){
         int x1 = c1.x();
         int y1 = c1.y();
+        if ( !this.board[x1][y1].getStatus() ) return false;
+        if(tileOnEdge(c1)) return true;
         Coordinates check = new Coordinates(x1+1,y1);
         if(
-                tileOnBoard(c1) || !getCell(check).getStatus() || ( getCell(check).getStatus() && getCell(check).getTile()==(null) )
+                !getCell(check).getStatus() || ( getCell(check).getStatus() && getCell(check).getTile()==(null) )
         ){return true;}
         Coordinates check1 = new Coordinates(x1-1,y1);
         if(
-                tileOnBoard(c1) || !getCell(check1).getStatus() || ( getCell(check1).getStatus() && getCell(check1).getTile()==(null) )
+                !getCell(check1).getStatus() || ( getCell(check1).getStatus() && getCell(check1).getTile()==(null) )
         ){return true;}
         Coordinates check2 = new Coordinates(x1,y1+1);
         if(
-                tileOnBoard(c1) || !getCell(check2).getStatus() || ( getCell(check2).getStatus() && getCell(check2).getTile()==(null) )
+                !getCell(check2).getStatus() || ( getCell(check2).getStatus() && getCell(check2).getTile()==(null) )
         ){return true;}
         Coordinates check3 = new Coordinates(x1,y1-1);
-        return tileOnBoard(c1) || !getCell(check3).getStatus() || ( getCell(check3).getStatus() && getCell(check3).getTile() == (null) );
+        return !getCell(check3).getStatus() || ( getCell(check3).getStatus() && getCell(check3).getTile() == (null) );
     }
     private boolean checkSpaceTile(Coordinates c1, Coordinates c2){
         boolean tile1 = checkSpaceTile(c1);
@@ -171,26 +176,44 @@ public class Board {
         int y1 = c1.y();
         int refill = 0;
         Coordinates check = new Coordinates(x1+1,y1);
-        if(
-                !getCell(check).getStatus() || getCell(check).getStatus() && getCell(check).getTile()==(null)
-        ){refill++;}
+        if(x1 != matrix_size-1){
+            if (!getCell(check).getStatus() || getCell(check).getStatus() && getCell(check).getTile()==(null))
+            {refill++;}
+        }else{
+            refill++;
+        }
         Coordinates check1 = new Coordinates(x1-1,y1);
-        if(
-                !getCell(check1).getStatus() || getCell(check1).getStatus() && getCell(check1).getTile()==(null)
-        ){refill++;}
+        if(x1 != 0){
+            if (!getCell(check1).getStatus() || getCell(check1).getStatus() && getCell(check1).getTile()==(null))
+            {refill++;}
+        }else{
+            refill++;
+        }
         Coordinates check2 = new Coordinates(x1,y1+1);
-        if(
-                !getCell(check2).getStatus() || getCell(check2).getStatus() && getCell(check2).getTile()==(null)
-        ){refill++;}
+        if(y1 != matrix_size-1){
+            if (!getCell(check2).getStatus() || getCell(check2).getStatus() && getCell(check2).getTile()==(null))
+            {refill++;}
+        }else{
+            refill++;
+        }
         Coordinates check3 = new Coordinates(x1,y1-1);
-        if( !getCell(check3).getStatus() || getCell(check3).getStatus() && getCell(check3).getTile() == (null)){refill++;}
+        if(x1 != 0){
+            if (!getCell(check1).getStatus() || getCell(check1).getStatus() && getCell(check1).getTile()==(null))
+            {refill++;}
+        }else{
+            refill++;
+        }
         return refill == 4;
-
     }
 
-    private boolean tileOnBoard(Coordinates c1){
+    private boolean tileOnEdge(Coordinates c1){
         int x1 = c1.x();
         int y1 = c1.y();
-        return x1 == matrix_size - 1 || y1 == matrix_size - 1;
+        return x1==0 || y1==0 || x1==matrix_size-1 || y1==matrix_size-1;
+    }
+
+    // method useful only for test
+    public void setTilesTaken(int tilesTaken) {
+        this.tilesTaken = tilesTaken;
     }
 }
