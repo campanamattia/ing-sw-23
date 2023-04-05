bb71b041660eaf4e397bb7e8abc14f3d4ac6f404
Mattia Campana <campana.mattia@outlook.com>
2023-04-05 02:11:42 +0200

    Implemented EndedMatch doRank() method

--- src/main/java/Server/Controller/Phase/EndedMatch.java
+++ src/main/java/Server/Controller/Phase/EndedMatch.java
@@ -6,9 +6,12 @@ import Server.Model.Rank;
 import java.util.List;
 
 public class EndedMatch{
-    private List<Rank> ranking = null;
+    private List<Rank> leaderboard = null;
 
     public List<Rank> doRank(List<Player> players){
-        return null;
+        players.sort((p1, p2) -> Integer.compare(p2.getScore(), p1.getScore()));
+        for(Player p : players)
+            this.leaderboard.add(new Rank(p.getID(), p.getScore()));
+        return this.leaderboard;
     }
 }
