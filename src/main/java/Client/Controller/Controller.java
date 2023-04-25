1b8b834a890e4a18526c9161c5c5313a8da659b1
matteobettiati <matteo.bet.38@gmail.com>
2023-04-25 12:01:32 +0200

    introduced more checks from input syntax.

--- src/main/java/Client/Controller/Controller.java
+++ src/main/java/Client/Controller/Controller.java
@@ -13,12 +13,15 @@ import Utils.Coordinates;
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO: 25/04/2023 :
+// 1) check the syntax of the input and in case throw Exception.
+// 2) adjust the view with method getInstant() that has to be implemented in the server.
+// 3) implement pingMessage, addPlayerMessage.
+// 4) check if is missing something and do tests.
+
+
 public class Controller {
     private View view;
-
-    // ...........................
-    // to review if I need a list of playerID
-    // ...........................
     private String playerID;
 
     public Controller() {
@@ -28,7 +31,7 @@ public class Controller {
         this.playerID = System.console().readLine();
     }
 
-    public void changeView(String view){
+    public void changeView(String view) {
         setView(ViewFactory.getView(view));
     }
 
@@ -38,11 +41,11 @@ public class Controller {
 
     public void inputReader() {
         // chose view
-        while(true){
+        while (true) {
             try {
                 buildMessage(System.console().readLine());
-            }catch (InvalidInputException e){
-                System.out.println("Input non valido!");
+            } catch (InvalidInputException e) {
+                System.out.println("Input not valid!");
             }
         }
     }
@@ -56,9 +59,8 @@ public class Controller {
         ClientMessage clientMessage;
         // command -body
         OperationType opType = assignOpType(input.split(" ")[0]);
-        if(opType.equals(OperationType.CHANGEVIEW)){
-            if( !(input.equals("CLI")) && !(input.equals("GUI")) )
-                throw new InvalidInputException(input);
+        checkInput(input.split("-")[1], opType);
+        if (opType.equals(OperationType.CHANGEVIEW)) {
             // cv -CLI/GUI
 
             // yet to finish, need to transform String view to View ..........
@@ -69,20 +71,21 @@ public class Controller {
 
             //......................
         }
-        if(opType.equals(OperationType.WRITEMESSAGE)){
+        if (opType.equals(OperationType.WRITEMESSAGE)) {
             // wm -message text
-            //................
-            // corner case " this is a-text
-            // ..................
-            String messageText = input.split("-")[1];
-            clientMessage = new WriteChatMessage(playerID,messageText);
+            int index = input.indexOf("-");
+            // tmpOpType is a helper variable useful just for the creation of messageText
+            String tmpOpType = input.substring(0, index).trim();
+            String messageText = input.substring(index + 1).trim();
+            clientMessage = new WriteChatMessage(playerID, messageText);
             return clientMessage;
         }
-        if(opType.equals(OperationType.SELECTEDTILES)){
+        if (opType.equals(OperationType.SELECTEDTILES)) {
             // st -x,y/x,y/x,y
             String coordinates = input.split("-")[1];
+
             List<Coordinates> selectedCoordinates = extractCoordinates(coordinates);
-            clientMessage = new SelectedTilesMessage(playerID,selectedCoordinates);
+            clientMessage = new SelectedTilesMessage(playerID, selectedCoordinates);
             return clientMessage;
         }
         // it cannot be anything bit a insertTile because it's checked in assignOpType.
@@ -90,12 +93,13 @@ public class Controller {
         String orderOfTiles = input.split("-")[1].split("/")[0];
         List<Integer> sorted = extractInteger(orderOfTiles);
         int column = Integer.parseInt(input.split("-")[1].split("/")[1]);
-        clientMessage = new InsertTilesMessage(opType,playerID,sorted,column);
+        clientMessage = new InsertTilesMessage(opType, playerID, sorted, column);
         return clientMessage;
     }
 
     /**
      * Turn the command from stream to enumeration.
+     *
      * @param input command inserted by the player.
      * @return Type of command inserted by a player in form of enumeration.
      * @throws InvalidInputException if the command chosen does not exist.
@@ -118,15 +122,16 @@ public class Controller {
 
     /**
      * Receive coordinate in shape of String and turn them into list of coordinates.
+     *
      * @param input Receive coordinate in shape of String.
      * @return list of coordinates.
      */
-    private List<Coordinates> extractCoordinates(String input){
+    private List<Coordinates> extractCoordinates(String input) {
         String[] tmp = input.split("/");
         List<Coordinates> coordinates = new ArrayList<>();
         Coordinates tmpCoordinates;
-        for(int i=0;i<tmp.length;i++){
-            tmpCoordinates = new Coordinates(Integer.parseInt(input.split("/")[i].split(";")[0]),Integer.parseInt(input.split("/")[i].split(";")[1]));
+        for (int i = 0; i < tmp.length; i++) {
+            tmpCoordinates = new Coordinates(Integer.parseInt(input.split("/")[i].split(";")[0]), Integer.parseInt(input.split("/")[i].split(";")[1]));
             coordinates.add(tmpCoordinates);
         }
         return coordinates;
@@ -134,15 +139,77 @@ public class Controller {
 
     /**
      * Turn input from String into a list of int.
+     *
      * @param input input of number in shape of string.
      * @return list of int.
      */
-    private List<Integer> extractInteger(String input){
+    private List<Integer> extractInteger(String input) {
         String[] tmp = input.split(",");
         List<Integer> orderOfTiles = new ArrayList<>();
         for (String s : tmp) {
             orderOfTiles.add(Integer.parseInt(s));
         }
         return orderOfTiles;
+    }
+
+    /**
+     * Checks if the input inserted by a user is valid.
+     * @param input String received.
+     * @param opType Type of operation.
+     * @throws InvalidInputException if the input is not valid.
+     */
+    private void checkInput(String input, OperationType opType) throws InvalidInputException {
+
+        // yet to check that input String must be "command -rest" and not for example "command--rest" ecc.
+
+        if (opType.equals(OperationType.CHANGEVIEW)) {
+            // cv -CLI/GUI
+            if (!(input.equals("CLI")) && !(input.equals("GUI")))
+                throw new InvalidInputException(input);
+        }
+        if (opType.equals(OperationType.INSERTTILES)) {
+            // it -x,y,z/c
+            String[] parts = input.split("/");
+            if (parts.length != 2) {
+                throw new InvalidInputException(input);
+            }
+            for (String p : parts) {
+                String[] coordinate = p.split(",");
+                if (coordinate.length > 3 || coordinate.length == 0) {
+                    throw new InvalidInputException(input);
+                }
+                try {
+                    for (String s : coordinate) {
+                        Integer.parseInt(s);
+                    }
+                } catch (NumberFormatException e) {
+                    throw new InvalidInputException(input);
+                }
+                try{
+                    Integer.parseInt(input.split("/")[1]);
+                }catch (NumberFormatException e){
+                    throw new InvalidInputException(input);
+                }
+            }
+        }
+        if (opType.equals(OperationType.SELECTEDTILES)) {
+            // st x,y/x,y/x,y
+            String[] parts = input.split("/");
+            if (parts.length > 3 || parts.length == 0) {
+                throw new InvalidInputException(input);
+            }
+            for (String p : parts) {
+                String[] coordinate = p.split(",");
+                if (coordinate.length != 2) {
+                    throw new InvalidInputException(input);
+                }
+                try {
+                    Integer.parseInt(coordinate[0]);
+                    Integer.parseInt(coordinate[1]);
+                } catch (NumberFormatException e) {
+                    throw new InvalidInputException(input);
+                }
+            }
+        }
     }
 }
