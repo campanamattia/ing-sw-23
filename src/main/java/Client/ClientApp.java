9d709fe4a9d80d294a2e007f9024723fb3a670d3
matteobettiati <matteo.bet.38@gmail.com>
2023-05-09 15:34:47 +0200

    fixing ViewFactory and ClientApp

--- src/main/java/Client/ClientApp.java
+++ src/main/java/Client/ClientApp.java
@@ -1,22 +1,14 @@
 package Client;
 
 import Client.View.*;
-import Client.Network.*;
-import Client.Controller.Controller;
+import java.util.Objects;
 
 public class ClientApp {
     public static void main(String[] args) {
-        View view;
-        Network network;
-        Controller controller;
-        do{
-            view = ViewFactory.getView(args[1]);
-            if(view == null)
-                System.out.println("++");
-            //wait a scan to save in args[1]
-        }while(view==null);
-        controller = new Controller(view);
-        //we now need to ask for the connection type and initialize the right network object
+
+        String viewName = Objects.requireNonNullElse(args[1], "CLI");
+        ViewFactory.instanceView(viewName);
 
     }
+
 }
