2a6304305999e10c8fd401d8437d1dcd94e4e7a4
matteobettiati <matteo.bet.38@gmail.com>
2023-06-26 20:33:44 +0200

    gui nearly done

--- src/main/java/Client/View/Gui/Scene/LivingRoom.java
+++ src/main/java/Client/View/Gui/Scene/LivingRoom.java
@@ -15,8 +15,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 import java.rmi.RemoteException;
